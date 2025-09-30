d6a8f7669dec93afe66a40373fd939caf94d84e0
Cristian <diazcristian0210@gmail.com>
2025-09-29 22:38:13 -0300

    Agregue a queries la logica para obtener desde la api que creamos en el front para poder editar el producto indicado

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -57,3 +57,19 @@ export const eliminarProductoAPI = async (id) => {
     console.error(error);
   }
 };
+
+//PUT, PATCH
+export const editarProducto = async (productoActualizado, id) => {
+  try {
+    const respuesta = await fetch("http://localhost:3000/productos/"+id,{
+        method: "PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(productoActualizado)
+    });
+    return respuesta
+  } catch (error) {
+    console.error(error);
+  }
+};
