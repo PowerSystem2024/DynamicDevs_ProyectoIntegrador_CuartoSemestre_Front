c80c3287cf7d67e67b7f46c4fb81ade7a790cf05
Cristian <diazcristian0210@gmail.com>
2025-10-23 11:35:39 -0300

    Mejoras en la interfaz del inicio cuando no está dado de alta el backend

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -2,21 +2,25 @@ import { Container, Row } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
 import { useEffect, useState } from "react";
 import { listarProductos } from "../helpers/queries";
-
+import "../../App.css"
 const Inicio = () => {
   const [productos, setProductos] = useState([]);
-
+  const [error, setError] = useState(false);
   useEffect(() => {
     obtenerProductos();
   }, []);
 
   const obtenerProductos = async () => {
-    const respuesta = await listarProductos();
-    if (respuesta.status === 200) {
-      const datos = await respuesta.json();
-      setProductos(datos);
-    } else {
-      alert("Error al listar los productos");
+    try {
+      const respuesta = await listarProductos();
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        setProductos(datos);
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      setError(true);
     }
   };
 
@@ -28,13 +32,19 @@ const Inicio = () => {
         alt="fondo alfajores"
       />
       <Container className="mt-5">
-        <h1 className="display-4 text-underline-warning">Nuestros Productos</h1>
+        <h1 className="display-4 text-underline-warning titulo-principal">Nuestros Productos</h1>
         <hr />
-        <Row>
-          {productos.map((producto) => (
-            <CardProducto key={producto._id} producto={producto} />
-          ))}
-        </Row>
+          {error || productos.length === 0 ? (
+          <h1 className="mt-5 fw-bold mensaje-carga">
+            Estamos trabajando cargando los productos. Apenas estén cargados se verán en esta página. Disculpen las molestias.
+          </h1>
+        ) : (
+          <Row>
+            {productos.map((producto) => (
+              <CardProducto key={producto._id} producto={producto} />
+            ))}
+          </Row>
+        )}
       </Container>
     </section>
   );
